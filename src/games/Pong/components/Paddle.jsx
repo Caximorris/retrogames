2df789a40ff15f5bd5cd46b72301e1bec1c3f4f4
Joan Fornés Rayó <yuami99@gmail.com>
2024-05-12 16:33:54 +0200

    feat(pong): paddle logic and animation
    updated paddle animation with frame animation, added relative position and sizing to the board, update position mapped to new board size

--- src/games/Pong/components/Paddle.jsx
+++ src/games/Pong/components/Paddle.jsx
@@ -1,61 +1,21 @@
 import PropTypes from 'prop-types';
-import { useState, useEffect, useRef } from 'react';
-
-const keyState = {}
+import usePaddle from "../logic/usePaddle.js";
 
 const Paddle = ({ boardSize, upKey, downKey, isPlayerOne }) => {
-  const [position, setPosition] = useState(Math.round(boardSize.height / 2))
-  const positionRef = useRef(position);
-  const gameTimeoutRef = useRef();
-
-  function getNextPaddlePosition(position, isGoingUp, boardSize) {
-    if (position.y > boardSize.height) {
-      return boardSize.height
-    }
-    if (position.y < 0) {
-      return 0
-    }
-    return position.y + (isGoingUp ? -20 : 20)
-  }
-
-  useEffect(() => {
-    const handleKeyDown = (e) => {
-      e.preventDefault();
-      keyState[e.key] = true;
-    };
-
-    const handleKeyUp = (e) => {
-      e.preventDefault();
-      keyState[e.key] = false;
-    };
-
-    window.addEventListener('keydown', handleKeyDown, true);
-    window.addEventListener('keyup', handleKeyUp, true);
-
-    const gameLoop = () => {
-      let nextPosition = positionRef.current;
-      if (keyState[upKey] || keyState[downKey]) {
-        nextPosition = getNextPaddlePosition(position, keyState[upKey], boardSize);
-      }
-
-      if (position !== nextPosition) {
-        setPosition(nextPosition);
-        positionRef.current = nextPosition;
-      }
-      gameTimeoutRef.current = setTimeout(gameLoop, 15);
-    };
-
-    gameLoop();
-
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-      window.removeEventListener('keyup', handleKeyUp);
-      clearTimeout(gameTimeoutRef.current)
-    };
-  }, [boardSize, downKey, upKey, position]);
+     const {position, size} = usePaddle(boardSize, upKey, downKey);
 
   return (
-    <div className="paddle" style={{ top: `${position}px`, left: isPlayerOne ? '5%' : '93%' }} />
+      <div
+          className="paddle"
+          style={{
+            top: `${position}px`,
+            left: isPlayerOne ? '5%' : '95%',
+            position: 'absolute',
+            width: `${size.width}px`,
+            height: `${size.height}px`,
+            backgroundColor: 'white',
+          }}
+      />
   );
 }
 
